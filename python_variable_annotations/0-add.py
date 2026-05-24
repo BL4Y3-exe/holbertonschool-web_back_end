6c070db82f746255a47f603ecfd9fcab4e85fef8
Ilknur <ilknurkazimov308@gmail.com>
2026-05-24 13:18:54 +0400

    Add 0-add.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module containt type-annotated function "add"."""
+
+
+def add(a:float, b:float) -> float:
+    """
+    Function takes a float "a" and float "b"
+    and returns their sum as a float."""
+    return a + b
