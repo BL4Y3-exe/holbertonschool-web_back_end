b80d5e8ff56cf0ebd9f52713a4e9145663c3bbc2
Ilknur <ilknurkazimov308@gmail.com>
2026-05-24 13:20:51 +0400

    fix path

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """This module containt type-annotated function "add"."""
 
 
 def add(a:float, b:float) -> float:
     """
     Function takes a float "a" and float "b"
-    and returns their sum as a float."""
+    and returns their sum as a float.
+    """
     return a + b
