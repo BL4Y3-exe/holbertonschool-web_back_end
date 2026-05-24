5c4c76d516c6d060b13d4d818d43a3ca4dae49fb
Ilknur <ilknurkazimov308@gmail.com>
2026-05-24 14:59:26 +0400

    Add 2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module contains "floor" function."""
+
+
+def floor(n: float) -> int:
+    """
+    Function takes a float n and returns its floor.
+    """
+    return int(n)
