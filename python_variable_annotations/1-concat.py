a9d9b8ca9bc0d29e7dfcfd1ecb2325837cd5839d
Ilknur <ilknurkazimov308@gmail.com>
2026-05-24 13:27:11 +0400

    Add 1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module contains type-annotated function "concat"."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function takes str1 and str2 (which are strings)
+    and returns a concatenated string.
+    """
+    return str1 + str2
