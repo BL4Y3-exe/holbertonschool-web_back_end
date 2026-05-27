c71ab97fdf0e074b4b9a7007add38b4ca55fb0c5
Ilknur <ilknurkazimov308@gmail.com>
2026-05-27 17:54:19 +0400

    Fix module's name

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,14 +5,17 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Function takes n and max_delay
-    and returns the list of all delays.
+    Spawns wait_random n times with the specified max_delay
+    and returns the list of delays in ascending order.
     """
-    tasks = [wait_random(max_delay) for i in range(n)]
+    tasks = [wait_random(max_delay) for _ in range(n)]
     delays = []
-    for task in asincio.as_completed(tasks):
+
+    for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
+
     return delays
