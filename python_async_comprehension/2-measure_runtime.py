5daed1be5f4363d2f8e0a790f7d2b3a04b8be9b2
Ilknur <ilknurkazimov308@gmail.com>
2026-05-29 22:57:17 +0400

    Fix asyncio.gather

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
 """This module contains measure_runtime coroutine."""
+#!/usr/bin/env python3
+"""This module contains measure_runtime coroutine."""
 import asyncio
 import time
-from typing import List
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """Executes async_comprehension four times in parallel using asyncio.gather,
-
     measures the total runtime, and returns it.
     """
     start_time = time.time()
 
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    tasks = [async_comprehension() for _ in range(4)]
+
+    await asyncio.gather(*tasks)
 
     end_time = time.time()
 
