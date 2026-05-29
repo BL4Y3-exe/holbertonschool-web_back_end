09a2325bd348d47ab32fba1887ed02fbf46a6895
Ilknur <ilknurkazimov308@gmail.com>
2026-05-29 22:50:18 +0400

    Add 1-async_comperhension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """This module contains async_comperhension coroutine."""
-import asyncio
 from typing import List
 
-async_generator = __import__('5-async_generator').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
