22259f3feab2b0bccc7e06f03b3ee26e9908fa6b
Ilknur <ilknurkazimov308@gmail.com>
2026-05-29 22:31:34 +0400

    Fix pycodestyle

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -11,11 +11,11 @@ def measure_time(n: int, max_delay: int) -> float:
     and returns total_time/n.
     """
     start_time = time()
-    
+
     run(wait_n(n, max_delay))
-    
+
     end_time = time()
-    
+
     total_time = end_time - start_time
-    
+
     return total_time / n
