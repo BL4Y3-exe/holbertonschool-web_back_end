1249aca629bbe821640b6e3abea6185f840f22e7
Ilknur <ilknurkazimov308@gmail.com>
2026-06-14 22:47:08 +0400

    Add 0-simple_hleper_function.py

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module contains index_range function."""
+
+def index_range(page, page_size):
+    """
+    Function returns a tuple of start and end indexes for pagination.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
